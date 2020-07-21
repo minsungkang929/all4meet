2854dd945a9ef392b9a5ea2b1bb577bf75b7aae5
Minsung Kang <minsung.kang929@gmail.com>
2020-07-21 19:14:21 -0400

    contact v0.1

--- src/Contact.js
+++ src/Contact.js
@@ -3,65 +3,65 @@ import React from "react"
 const Contact = () => {
 
     return(
-        <div id="Contact" class="contact-area bg-light pt-100">
-            <div class="container">
-                <div class="section-title text-center">
-                    <h2>get in touch</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <div id="Contact" className="contact-area bg-light pt-100">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h2>Get in touch</h2>
+                    <p>Give any questions, suggestions or support. Thank you.</p>
                 </div>
-                <div class="row">
-                    <div class="offset-lg-2 col-lg-8 text-center">
-                        <div class="contact-from">
+                <div className="row">
+                    <div className="offset-lg-2 col-lg-8 text-center">
+                        <div className="contact-from">
                             <form id="contact-form" action="mail.php" method="post">
                                 <input name="name" type="text" placeholder="Name" />
                                 <input name="email" type="text" placeholder="Email" />
                                 <textarea name="message" placeholder="Your message"></textarea>
-                                <input class="submit" type="submit" value="SUBMIT" />
+                                <input className="submit" type="submit" value="SUBMIT" />
                             </form>
-                            <p class="form-messege"></p>
+                            <p className="form-messege"></p>
                         </div>
                     </div>
                 </div>
-                <div class="conatct-info fix">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 text-center">
-                            <div class="single-contact-info">
-                                <div class="contact-icon">
-                                    <i class="fa fa-phone" aria-hidden="true"></i>
+                <div className="conatct-info fix">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-4 text-center">
+                            <div className="single-contact-info">
+                                <div className="contact-icon">
+                                    <i className="fa fa-phone" aria-hidden="true"></i>
                                 </div>
-                                <div class="contact-text">
+                                <div className="contact-text">
                                     <span>
-                                        +88092 (1) 5184203
+                                        +1 (123) 123-xxxx
                                         <br />
-                                        +00 (22) 225455565
+                                        +1 (123) 123-zzzz
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-4 text-center">
-                            <div class="single-contact-info res-xs2">
-                                <div class="contact-icon">
-                                    <i class="fa fa-globe" aria-hidden="true"></i>
+                        <div className="col-lg-4 col-md-4 text-center">
+                            <div className="single-contact-info res-xs2">
+                                <div className="contact-icon">
+                                    <i className="fa fa-globe" aria-hidden="true"></i>
                                 </div>
-                                <div class="contact-text">
+                                <div className="contact-text">
                                     <span>
-                                        <a href="#">info@devitems.com</a>
+                                        <a href="#">info@all4meet.com</a>
                                             <br />
-                                        <a href="#">devitems@email.com</a>
+                                        <a href="#">help@all4meet.com</a>
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-4 text-center">
-                            <div class="single-contact-info">
-                                <div class="contact-icon">
-                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        <div className="col-lg-4 col-md-4 text-center">
+                            <div className="single-contact-info">
+                                <div className="contact-icon">
+                                    <i className="fa fa-map-marker" aria-hidden="true"></i>
                                 </div>
-                                <div class="contact-text">
+                                <div className="contact-text">
                                     <span>
-                                        ur address goes here,
+                                        123 Heaven Street, CloudCity,
                                         <br />
-                                        street,Crossroad123.
+                                        Sky, God.
                                     </span>
                                 </div>
                             </div>
